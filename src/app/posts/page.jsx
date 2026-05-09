6d91980401f16e99c0e1fa747fccb7a4aa34373d
probillal <smbillalhossain273@gmail.com>
2026-05-09 21:37:24 +0600

    different types of data fetching in nextjs

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,16 +1,16 @@
-// const getPosts = async () => {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-//   return res.json();
-// };
-
 const getPosts = async () => {
-  try {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    return res.json();
-  } catch (error) {
-    throw new Error("Failed to fetch posts");
-  }
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return res.json();
 };
+
+// const getPosts = async () => {
+//   try {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     return res.json();
+//   } catch (error) {
+//     throw new Error("Failed to fetch posts");
+//   }
+// };
 const PostsPage = async () => {
   //   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   //   const posts = await res.json();
